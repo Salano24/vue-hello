25c72dad582dd6b56973a17cdb6faa261d8541fc
Salano24 <salernomarco48@email.com>
2022-10-26 14:37:06 +0200

    bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            message: 'CIAO',
+            message: 'UN SALUTO DA NAGATOMO',
+            image: "https://icdn.sempreinter.com/wp-content/uploads/2015/08/nagatomo-1024x683.jpg" ,
+            description: 'Nagatomo che applaude' ,
+            textColor: 'color',
+            sfondo: 'blue',
+            grandezza: 'pic'
         }
     },
+    
 }).mount('#app')
